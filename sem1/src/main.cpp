82b2204822995ad59f88ebf59702a85a76d363aa
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-02-16 18:09:52 +0100

    sem1 init

---
+++ sem1/src/main.cpp
@@ -0,0 +1,18 @@
+#include <print>
+#include <string_view>
+
+using namespace std::literals;
+
+int main(int argc, char **argv) {
+  bool parallel = false;
+
+  for (int i = 1; i < argc; ++i) {
+    if (argv[i] == "--parallel"sv) {
+      parallel = true;
+    }
+  }
+
+  std::print("{}", parallel ? "Is parallel" : "Is serial");
+
+  return 0;
+}
