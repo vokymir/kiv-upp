62f8a9fe6d01f6d6162d3d84d01ca86bbef53685
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-02-16 15:46:28 +0100

    c01 b,c + other

--- c01/cw/b/main.cpp
+++ c01/cw/b/main.cpp
@@ -1,100 +1,127 @@
 #include <iostream>
 
-#include <thread>
-#include <chrono>
 #include <algorithm>
-#include <vector>
+#include <chrono>
 #include <future>
+#include <thread>
+#include <vector>
 
-// nejaka relativne neuzitecna funkce, ktera vygeneruje pole cisel, seradi ho a vrati nahodny prvek z nej
-int merena_funkce_1()
-{
-	std::vector<int> numbers(1000000);
+// nejaka relativne neuzitecna funkce, ktera vygeneruje pole cisel, seradi ho a
+// vrati nahodny prvek z nej
+int merena_funkce_1() {
+  std::vector<int> numbers(10000000);
 
-	auto f = []() -> int { return rand() % 10000 - 5000; };
+  auto f = []() -> int { return rand() % 10000 - 5000; };
 
-	std::generate(numbers.begin(), numbers.end(), f);
-	std::sort(numbers.begin(), numbers.end());
+  std::generate(numbers.begin(), numbers.end(), f);
+  std::sort(numbers.begin(), numbers.end());
 
-	return numbers[rand() % numbers.size()];
+  return numbers[rand() % numbers.size()];
 }
 
-// nejaka relativne neuzitecna funkce, ktera vygeneruje pole cisel, seradi ho a vrati nahodny prvek z nej
-int merena_funkce_2()
-{
-	std::vector<int> numbers(1000000);
+// nejaka relativne neuzitecna funkce, ktera vygeneruje pole cisel, seradi ho a
+// vrati nahodny prvek z nej
+int merena_funkce_2() {
+  std::vector<int> numbers(10000000);
 
-	auto f = []() -> int { return rand() % 100000 - 50000; };
+  auto f = []() -> int { return rand() % 100000 - 50000; };
 
-	std::generate(numbers.begin(), numbers.end(), f);
-	std::sort(numbers.begin(), numbers.end());
+  std::generate(numbers.begin(), numbers.end(), f);
+  std::sort(numbers.begin(), numbers.end());
 
-	return numbers[rand() % numbers.size()];
+  return numbers[rand() % numbers.size()];
 }
 
-int main(int argc, char** argv)
-{
-	int result1, result2, result;
-	std::chrono::steady_clock::time_point start, end;
-	long long time_ms;
-	srand(static_cast<unsigned int>(time(nullptr)));
+int main(int argc, char **argv) {
+  int result1, result2, result;
+  std::chrono::steady_clock::time_point start, end;
+  long long time_ms;
+  srand(static_cast<unsigned int>(time(nullptr)));
 
-	/*
-	 * Ukol pro vas: pocitat zrychleni z jednoho behu neni "fer" - na systemu s preemptivnim planovacem, na kterem navic bezi spousty
-	 * nejen systemovych sluzeb a jinych aplikaci, jsou casy ruzne. Provedte totez mereni pro kazdou z metod vicekrat a vypoctete
-	 * urychleni 2. a 3. zpusobu vuci 1. Pokuste se zduvodnit namerene/vypoctene hodnoty.
-	 */
+  /*
+   * Ukol pro vas: pocitat zrychleni z jednoho behu neni "fer" - na systemu s
+   * preemptivnim planovacem, na kterem navic bezi spousty nejen systemovych
+   * sluzeb a jinych aplikaci, jsou casy ruzne. Provedte totez mereni pro kazdou
+   * z metod vicekrat a vypoctete urychleni 2. a 3. zpusobu vuci 1. Pokuste se
+   * zduvodnit namerene/vypoctene hodnoty.
+   */
 
-	//// Mereni 1 - bez paralelizace
-	{
-		start = std::chrono::steady_clock::now();
+  //// Mereni 1 - bez paralelizace
+  const int runs = 10;
+  long long sum = 0;
+  for (auto i = 0; i < runs; i++) {
+    start = std::chrono::steady_clock::now();
 
-		result1 = merena_funkce_1();
-		result2 = merena_funkce_2();
-		result = result1 + result2;
+    result1 = merena_funkce_1();
+    result2 = merena_funkce_2();
+    result = result1 + result2;
 
-		end = std::chrono::steady_clock::now();
-		time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    end = std::chrono::steady_clock::now();
+    time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+                  .count();
+    sum += time_ms;
 
-		std::cout << "(1) Result: " << result << " in " << time_ms << "ms" << std::endl;
-	}
+    // std::cout << "(1) Result: " << result << " in " << time_ms << "ms"
+    //           << std::endl;
+  }
+  double avg = sum / static_cast<double>(runs);
+  std::cout << "(1) in 10 runs: " << avg << std::endl;
+  double first = avg;
 
-	//// Mereni 2 - thread + join
-	{
-		start = std::chrono::steady_clock::now();
+  //// Mereni 2 - thread + join
+  sum = 0;
+  for (auto i = 0; i < runs; i++) {
+    start = std::chrono::steady_clock::now();
 
-		std::thread calc_thread([&result1]() {
-			result1 = merena_funkce_1();
-		});
-		result2 = merena_funkce_2();
+    std::thread calc_thread([&result1]() { result1 = merena_funkce_1(); });
+    result2 = merena_funkce_2();
 
-		if (calc_thread.joinable())
-			calc_thread.join();
+    if (calc_thread.joinable())
+      calc_thread.join();
 
-		result = result1 + result2;
+    result = result1 + result2;
 
-		end = std::chrono::steady_clock::now();
-		time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    end = std::chrono::steady_clock::now();
+    time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+                  .count();
 
-		std::cout << "(2) Result: " << result << " in " << time_ms << "ms" << std::endl;
-	}
+    sum += time_ms;
+    // std::cout << "(2) Result: " << result << " in " << time_ms << "ms"
+    //           << std::endl;
+  }
+  avg = sum / static_cast<double>(runs);
+  std::cout << "(2) in 10 runs: " << avg << std::endl;
+  double second = avg;
 
-	//// Mereni 3 - async
-	{
-		start = std::chrono::steady_clock::now();
+  //// Mereni 3 - async
+  sum = 0;
+  for (auto i = 0; i < runs; i++) {
+    start = std::chrono::steady_clock::now();
 
-		auto res1_future = std::async(std::launch::async, &merena_funkce_1);
-		result2 = merena_funkce_2();
+    auto res1_future = std::async(std::launch::async, &merena_funkce_1);
+    result2 = merena_funkce_2();
 
-		result1 = res1_future.get();
+    result1 = res1_future.get();
 
-		result = result1 + result2;
+    result = result1 + result2;
 
-		end = std::chrono::steady_clock::now();
-		time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    end = std::chrono::steady_clock::now();
+    time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+                  .count();
 
-		std::cout << "(3) Result: " << result << " in " << time_ms << "ms" << std::endl;
-	}
+    sum += time_ms;
+    // std::cout << "(3) Result: " << result << " in " << time_ms << "ms"
+    //           << std::endl;
+  }
+  avg = sum / static_cast<double>(runs);
+  std::cout << "(3) in 10 runs: " << avg << std::endl;
+  double third = avg;
 
-	return 0;
+  double fst_to_scd = first / second;
+  double fst_to_thd = first / third;
+
+  std::cout << "1/2 = " << fst_to_scd << std::endl;
+  std::cout << "1/3 = " << fst_to_thd << std::endl;
+
+  return 0;
 }
--- c01/main.cpp
+++ c01/main.cpp
@@ -1,2 +1,18 @@
+#include <print>
+#include <thread>
 
-int main(int argc, char **argv) { return 0; }
+int main(int argc, char **argv) {
+
+  std::thread t1([]() {
+    for (int i = 0; i < 100; ++i)
+      std::print("A");
+  });
+
+  if (t1.joinable())
+    t1.join();
+
+  for (int i = 0; i < 100; ++i)
+    std::print("B");
+
+  return 0;
+}
