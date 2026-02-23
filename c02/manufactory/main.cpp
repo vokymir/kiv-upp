5cd8eb68fdd2f0c29e20266585a00a74b9bbc5ee
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-02-23 16:58:07 +0100

    c02 manufaktura done

--- c02/manufactory/main.cpp
+++ c02/manufactory/main.cpp
@@ -1,122 +1,143 @@
+#include <cstddef>
 #include <iostream>
 
-#include "router.h"
+#include "drawing.h"
 #include "input.h"
 #include "optimizer.h"
-#include "slicer.h"
-#include "drawing.h"
 #include "output.h"
+#include "router.h"
+#include "slicer.h"
 
 #include <chrono>
+#include <vector>
 
 using measurement_clock = std::chrono::steady_clock;
 using time_point = measurement_clock::time_point;
 
-time_point start_measurement()
-{
-	return measurement_clock::now();
+time_point start_measurement() { return measurement_clock::now(); }
+
+long end_measurement(const std::string &task_name, const time_point &start) {
+  auto diff = measurement_clock::now() - start;
+  long time =
+      std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
+  std::cout << "Uloha " << task_name << ": " << time << "ms" << std::endl;
+  return time;
 }
 
-void end_measurement(const std::string& task_name, const time_point& start)
-{
-	auto diff = measurement_clock::now() - start;
-	std::cout << "Uloha " << task_name << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count() << "ms" << std::endl;
-}
+int main(int argc, char **argv) {
+  CInput input;
+  CRouter router;
+  COptimizer optimizer;
+  CSlicer slicer;
+  CDrawing drawer;
+  COutput output;
 
-int main(int argc, char** argv)
-{
-	CInput input;
-	CRouter router;
-	COptimizer optimizer;
-	CSlicer slicer;
-	CDrawing drawer;
-	COutput output;
+  time_point tp;
+  time_point total_tp;
 
-	time_point tp;
-	time_point total_tp;
+  if (!router.Load_Graph("graph.txt")) {
+    std::cerr << "Nelze nacist graf stredisek (graph.txt)." << std::endl;
+    return 1;
+  }
+  constexpr size_t Number_Repeats = 10;
+  constexpr size_t Number_Tasks = 8; // A-H
+  std::vector<std::vector<long>> repeats;
+  repeats.resize(Number_Tasks, std::vector<long>(Number_Repeats, 0));
 
-	if (!router.Load_Graph("graph.txt")) {
-		std::cerr << "Nelze nacist graf stredisek (graph.txt)." << std::endl;
-		return 1;
-	}
-	
-	// A) prijeti a validace pozadavku
-	while (input.Accept_Request())
-	{
-		total_tp = start_measurement();
+  // A) prijeti a validace pozadavku
+  while (input.Accept_Request()) {
 
-		tp = start_measurement();
+    for (auto rep = 0; rep < Number_Repeats; rep++) {
+      std::cout << "A JEDEME, cislo: " << rep + 1 << "/" << Number_Repeats
+                << std::endl;
 
-		// B) vygenerovani "soucastky" (gen. algoritmus)
-		if (!optimizer.Optimize(input.Get_Diameters()))
-		{
-			std::cerr << "Nelze optimalizovat." << std::endl;
-			continue;
-		}
+      total_tp = start_measurement();
 
-		end_measurement("B", tp);
-		tp = start_measurement();
+      tp = start_measurement();
 
-		// C) slicing pro vyrobu na 3D tiskarne
-		if (!slicer.Slice(optimizer.Get_Optimized_Points()))
-		{
-			std::cerr << "Nelze vygenerovat GCode." << std::endl;
-			continue;
-		}
+      // B) vygenerovani "soucastky" (gen. algoritmus)
+      if (!optimizer.Optimize(input.Get_Diameters())) {
+        std::cerr << "Nelze optimalizovat." << std::endl;
+        continue;
+      }
 
-		end_measurement("C", tp);
-		tp = start_measurement();
+      repeats[1][rep] = end_measurement("B", tp);
+      tp = start_measurement();
 
-		// D) vykresleni pudorysu do souboru (SVG)
-		if (!drawer.Generate_Drawing(optimizer.Get_Optimized_Points()))
-		{
-			std::cerr << "Nelze vykreslit pudorys." << std::endl;
-			continue;
-		}
+      // C) slicing pro vyrobu na 3D tiskarne
+      if (!slicer.Slice(optimizer.Get_Optimized_Points())) {
+        std::cerr << "Nelze vygenerovat GCode." << std::endl;
+        continue;
+      }
 
-		end_measurement("D", tp);
-		tp = start_measurement();
+      repeats[2][rep] = end_measurement("C", tp);
+      tp = start_measurement();
 
-		// E) pokus o zlepseni poradi uzlu stredisek k expedici
-		std::list<size_t> nodes = input.Get_Target_Nodes();
-		if (router.Optimize_Node_Order(nodes, 1))
-			nodes = router.Get_Optimized_Node_Order();
+      // D) vykresleni pudorysu do souboru (SVG)
+      if (!drawer.Generate_Drawing(optimizer.Get_Optimized_Points())) {
+        std::cerr << "Nelze vykreslit pudorys." << std::endl;
+        continue;
+      }
 
-		end_measurement("E", tp);
-		tp = start_measurement();
+      repeats[3][rep] = end_measurement("D", tp);
+      tp = start_measurement();
 
-		// F) vygenerovani cesty pres N uzlu (stredisek)
-		if (!router.Generate_Path(nodes, 1))
-		{
-			std::cerr << "Nelze vygenerovat cestu." << std::endl;
-			continue;
-		}
+      // E) pokus o zlepseni poradi uzlu stredisek k expedici
+      std::list<size_t> nodes = input.Get_Target_Nodes();
+      if (router.Optimize_Node_Order(nodes, 1))
+        nodes = router.Get_Optimized_Node_Order();
 
-		end_measurement("F", tp);
-		tp = start_measurement();
+      repeats[4][rep] = end_measurement("E", tp);
+      tp = start_measurement();
 
-		// G) vykresleni mapy, kudy dodavka pojede
-		if (!router.Draw_Path())
-		{
-			std::cerr << "Nelze vykreslit mapu cestu." << std::endl;
-			continue;
-		}
+      // F) vygenerovani cesty pres N uzlu (stredisek)
+      if (!router.Generate_Path(nodes, 1)) {
+        std::cerr << "Nelze vygenerovat cestu." << std::endl;
+        continue;
+      }
 
-		end_measurement("G", tp);
-		tp = start_measurement();
+      repeats[5][rep] = end_measurement("F", tp);
+      tp = start_measurement();
 
-		// H) expedice (finalizace reportu do souboru)
-		if (!output.Write_Output(slicer.Get_GCode(), drawer.Get_SVG(), router.Get_Path(), router.Get_Total_Cost(), router.Get_Path_Drawing(), [&router](size_t id) -> const std::string& { return router.Get_Node_Name(id); }))
-		{
-			std::cerr << "Nelze zapsat vysledky." << std::endl;
-			continue;
-		}
+      // G) vykresleni mapy, kudy dodavka pojede
+      if (!router.Draw_Path()) {
+        std::cerr << "Nelze vykreslit mapu cestu." << std::endl;
+        continue;
+      }
 
-		end_measurement("H", tp);
-		end_measurement("Total", total_tp);
+      repeats[6][rep] = end_measurement("G", tp);
+      tp = start_measurement();
 
-		std::cout << "Pozadavek byl zpracovan." << std::endl;
-	}
+      // H) expedice (finalizace reportu do souboru)
+      if (!output.Write_Output(slicer.Get_GCode(), drawer.Get_SVG(),
+                               router.Get_Path(), router.Get_Total_Cost(),
+                               router.Get_Path_Drawing(),
+                               [&router](size_t id) -> const std::string & {
+                                 return router.Get_Node_Name(id);
+                               })) {
+        std::cerr << "Nelze zapsat vysledky." << std::endl;
+        continue;
+      }
 
-	return 0;
+      repeats[7][rep] = end_measurement("H", tp);
+      end_measurement("Total", total_tp);
+    }
+
+    std::cout << "Pozadavek byl zpracovan." << std::endl;
+
+    std::cout << "Number of runs: " << Number_Repeats << std::endl;
+    int i = 0;
+    for (const auto &task_times : repeats) {
+      long sum = 0;
+      for (const auto &task_time : task_times) {
+        sum += task_time;
+      }
+      double avg = static_cast<double>(sum) / Number_Repeats;
+
+      std::cout << (char)(65 + i) << " avg: " << avg << std::endl;
+      ++i;
+    }
+  }
+
+  return 0;
 }
--- c02/manufactory/router.cpp
+++ c02/manufactory/router.cpp
@@ -1,318 +1,311 @@
 #include "router.h"
 
-#include <fstream>
-#include <sstream>
 #include <algorithm>
-#include <set>
-#include <iostream>
 #include <cmath>
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <sstream>
 
-namespace
-{
-	// faze nacitani souboru
-	enum class NLoading_Phase
-	{
-		Unknown,
-		Nodes,
-		Edges
-	};
+namespace {
+// faze nacitani souboru
+enum class NLoading_Phase { Unknown, Nodes, Edges };
+} // namespace
+
+bool CRouter::Load_Graph(const std::string &file) {
+  std::ifstream ifs(file);
+
+  if (!ifs.is_open()) {
+    std::cout << "Cannot be opened..." << std::endl;
+    return false;
+  }
+
+  NLoading_Phase phase = NLoading_Phase::Unknown;
+
+  std::string line;
+  while (std::getline(ifs, line)) {
+    if (line == "NODES") {
+      phase = NLoading_Phase::Nodes;
+      continue;
+    } else if (line == "EDGES") {
+      phase = NLoading_Phase::Edges;
+      continue;
+    }
+
+    if (phase == NLoading_Phase::Nodes) {
+      std::string token;
+      std::istringstream iss(line);
+
+      try {
+        Graph_Node node;
+
+        if (!std::getline(iss, token, ';')) {
+          std::cout << "node #1" << std::endl;
+          return false;
+        }
+
+        node.id = std::stoull(token);
+
+        if (!std::getline(iss, token, ';')) {
+          std::cout << "node #2" << std::endl;
+          return false;
+        }
+
+        node.x = std::stod(token);
+
+        if (!std::getline(iss, token, ';')) {
+          std::cout << "node #3" << std::endl;
+          return false;
+        }
+
+        node.y = std::stod(token);
+
+        if (!std::getline(iss, token, ';')) {
+          std::cout << "node #4" << std::endl;
+          return false;
+        }
+
+        node.name = token;
+
+        m_nodes[node.id] = node;
+      } catch (std::exception &ex) {
+        std::cerr << "Chyba pri nacitani uzlu: " << ex.what() << std::endl;
+        return false;
+      }
+    } else if (phase == NLoading_Phase::Edges) {
+      std::string token;
+      std::istringstream iss(line);
+
+      try {
+        Graph_Edge edge;
+
+        if (!std::getline(iss, token, ';')) {
+          std::cout << "edge #1" << std::endl;
+          return false;
+        }
+
+        edge.src_id = std::stoull(token);
+
+        if (!std::getline(iss, token, ';')) {
+          std::cout << "edge #2" << std::endl;
+          return false;
+        }
+
+        edge.dest_id = std::stoull(token);
+
+        if (!std::getline(iss, token, ';')) {
+          std::cout << "edge #3" << std::endl;
+          return false;
+        }
+
+        edge.cost = std::stod(token);
+
+        m_nodes[edge.src_id].edges.push_back(edge);
+
+        std::swap(edge.src_id, edge.dest_id);
+
+        m_nodes[edge.src_id].edges.push_back(edge);
+      } catch (std::exception &ex) {
+        std::cerr << "Chyba pri nacitani hrany: " << ex.what() << std::endl;
+        return false;
+      }
+    } else if (phase == NLoading_Phase::Unknown) {
+      std::cout << "Unknown phase" << std::endl;
+      return false;
+    }
+  }
+
+  return true;
 }
 
-bool CRouter::Load_Graph(const std::string& file)
-{
-	std::ifstream ifs(file);
+bool CRouter::Optimize_Node_Order(const std::list<size_t> &nodeIds,
+                                  size_t inputNode) {
+  std::set<size_t> pending(nodeIds.begin(), nodeIds.end());
 
-	if (!ifs.is_open())
-		return false;
+  m_optimizedNodeOrder.clear();
 
-	NLoading_Phase phase = NLoading_Phase::Unknown;
+  auto get_distance = [this](size_t nsrc, size_t ndst) {
+    return std::sqrt(std::pow(m_nodes[nsrc].x - m_nodes[ndst].x, 2.0) +
+                     std::pow(m_nodes[nsrc].y - m_nodes[ndst].y, 2.0));
+  };
 
-	std::string line;
-	while (std::getline(ifs, line))
-	{
-		if (line == "NODES")
-		{
-			phase = NLoading_Phase::Nodes;
-			continue;
-		}
-		else if (line == "EDGES")
-		{
-			phase = NLoading_Phase::Edges;
-			continue;
-		}
+  size_t currentNode = inputNode;
+  while (!pending.empty()) {
+    size_t bestNode = InvalidNode;
+    double bestMetric = std::numeric_limits<double>::max();
+    for (auto &id : pending) {
+      auto dist = get_distance(currentNode, id);
+      if (dist < bestMetric) {
+        bestNode = id;
+        bestMetric = dist;
+      }
+    }
 
-		if (phase == NLoading_Phase::Nodes)
-		{
-			std::string token;
-			std::istringstream iss(line);
+    if (bestNode == InvalidNode)
+      return false;
 
-			try
-			{
-				Graph_Node node;
+    pending.erase(bestNode);
+    m_optimizedNodeOrder.push_back(bestNode);
 
-				if (!std::getline(iss, token, ';'))
-					return false;
+    currentNode = bestNode;
+  }
 
-				node.id = std::stoull(token);
-
-				if (!std::getline(iss, token, ';'))
-					return false;
-
-				node.x = std::stod(token);
-
-				if (!std::getline(iss, token, ';'))
-					return false;
-
-				node.y = std::stod(token);
-
-				if (!std::getline(iss, token))
-					return false;
-
-				node.name = token;
-
-				m_nodes[node.id] = node;
-			}
-			catch (std::exception& ex)
-			{
-				std::cerr << "Chyba pri nacitani uzlu: " << ex.what() << std::endl;
-				return false;
-			}
-		}
-		else if (phase == NLoading_Phase::Edges)
-		{
-			std::string token;
-			std::istringstream iss(line);
-
-			try
-			{
-				Graph_Edge edge;
-
-				if (!std::getline(iss, token, ';'))
-					return false;
-
-				edge.src_id = std::stoull(token);
-
-				if (!std::getline(iss, token, ';'))
-					return false;
-
-				edge.dest_id = std::stoull(token);
-
-				if (!std::getline(iss, token, ';'))
-					return false;
-
-				edge.cost = std::stod(token);
-
-				m_nodes[edge.src_id].edges.push_back(edge);
-
-				std::swap(edge.src_id, edge.dest_id);
-
-				m_nodes[edge.src_id].edges.push_back(edge);
-			}
-			catch (std::exception& ex)
-			{
-				std::cerr << "Chyba pri nacitani hrany: " << ex.what() << std::endl;
-				return false;
-			}
-		}
-		else if (phase == NLoading_Phase::Unknown)
-		{
-			return false;
-		}
-	}
-
-	return true;
+  return true;
 }
 
-bool CRouter::Optimize_Node_Order(const std::list<size_t>& nodeIds, size_t inputNode)
-{
-	std::set<size_t> pending(nodeIds.begin(), nodeIds.end());
+bool CRouter::Generate_Path(const std::list<size_t> nodeIds, size_t inputNode) {
+  auto clear_temps = [this]() {
+    for (auto &n : m_nodes) {
+      n.second.discovered_from = InvalidNode;
+      n.second.path_metric = std::numeric_limits<double>::max();
+    }
+  };
 
-	m_optimizedNodeOrder.clear();
+  auto find_path = [this, &clear_temps](size_t from, size_t to) {
+    clear_temps();
 
-	auto get_distance = [this](size_t nsrc, size_t ndst) {
-		return std::sqrt(std::pow(m_nodes[nsrc].x - m_nodes[ndst].x, 2.0) + std::pow(m_nodes[nsrc].y - m_nodes[ndst].y, 2.0));
-	};
+    std::list<size_t> discovered{from};
+    m_nodes[from].path_metric = 0;
 
-	size_t currentNode = inputNode;
-	while (!pending.empty())
-	{
-		size_t bestNode = InvalidNode;
-		double bestMetric = std::numeric_limits<double>::max();
-		for (auto& id : pending)
-		{
-			auto dist = get_distance(currentNode, id);
-			if (dist < bestMetric)
-			{
-				bestNode = id;
-				bestMetric = dist;
-			}
-		}
+    while (!discovered.empty()) {
+      size_t selected = discovered.front();
+      size_t next = InvalidNode;
+      double nextcost = std::numeric_limits<double>::max();
 
-		if (bestNode == InvalidNode)
-			return false;
+      for (size_t s : discovered) {
+        for (auto &e : m_nodes[s].edges) {
+          if (m_nodes[e.dest_id].discovered_from == InvalidNode &&
+              m_nodes[s].path_metric + e.cost < nextcost) {
+            selected = s;
+            next = e.dest_id;
+            nextcost = m_nodes[s].path_metric + e.cost;
+            break;
+          }
+        }
+      }
 
-		pending.erase(bestNode);
-		m_optimizedNodeOrder.push_back(bestNode);
+      for (auto itr = discovered.begin(); itr != discovered.end();) {
+        bool hasedges = false;
+        for (auto &e : m_nodes[*itr].edges) {
+          if (m_nodes[e.dest_id].discovered_from == InvalidNode) {
+            hasedges = true;
+            break;
+          }
+        }
 
-		currentNode = bestNode;
-	}
+        if (!hasedges)
+          itr = discovered.erase(itr);
+        else
+          ++itr;
+      }
 
-	return true;
+      if (next != InvalidNode) {
+        discovered.push_back(next);
+        m_nodes[next].discovered_from = selected;
+        m_nodes[next].path_metric = nextcost;
+
+        if (next == to)
+          break;
+      }
+    }
+
+    if (m_nodes[to].discovered_from == InvalidNode)
+      return Path();
+
+    Path pt;
+    pt.total_cost = m_nodes[to].path_metric;
+
+    pt.prepend(to);
+
+    while (pt.node_ids.front() != from)
+      pt.prepend(m_nodes[pt.node_ids.front()].discovered_from);
+
+    return pt;
+  };
+
+  Path total;
+  total.total_cost = 0;
+
+  size_t curNode = inputNode;
+  for (size_t node : nodeIds) {
+    auto res = find_path(curNode, node);
+    curNode = node;
+
+    if (res.empty())
+      continue;
+
+    total += res;
+  }
+
+  Path best = total;
+
+  m_path = best.node_ids;
+  m_totalCost = best.total_cost;
+
+  m_warehouses = std::set<size_t>(nodeIds.begin(), nodeIds.end());
+  m_internodes = std::set<size_t>(m_path.begin(), m_path.end());
+
+  return true;
 }
 
-bool CRouter::Generate_Path(const std::list<size_t> nodeIds, size_t inputNode)
-{
-	auto clear_temps = [this]() {
-		for (auto& n : m_nodes)
-		{
-			n.second.discovered_from = InvalidNode;
-			n.second.path_metric = std::numeric_limits<double>::max();
-		}
-	};
+bool CRouter::Draw_Path() {
+  if (m_warehouses.empty() || m_internodes.empty() || m_path.empty())
+    return false;
 
-	auto find_path = [this, &clear_temps](size_t from, size_t to) {
-		clear_temps();
+  auto is_warehouse = [this](const size_t id) {
+    return (m_warehouses.find(id) != m_warehouses.end());
+  };
+  auto is_internode = [this](const size_t id) {
+    return (m_internodes.find(id) != m_internodes.end());
+  };
 
-		std::list<size_t> discovered{ from };
-		m_nodes[from].path_metric = 0;
+  std::ostringstream svg_out;
 
-		while (!discovered.empty())
-		{
-			size_t selected = discovered.front();
-			size_t next = InvalidNode;
-			double nextcost = std::numeric_limits<double>::max();
+  constexpr size_t Grid_Size_X = 100;
+  constexpr size_t Grid_Size_Y = 100;
 
-			for (size_t s : discovered)
-			{
-				for (auto& e : m_nodes[s].edges)
-				{
-					if (m_nodes[e.dest_id].discovered_from == InvalidNode && m_nodes[s].path_metric + e.cost < nextcost)
-					{
-						selected = s;
-						next = e.dest_id;
-						nextcost = m_nodes[s].path_metric + e.cost;
-						break;
-					}
-				}
-			}
+  constexpr double Grid_Spacing_X = 1.0;
+  constexpr double Grid_Spacing_Y = 1.0;
 
-			for (auto itr = discovered.begin(); itr != discovered.end(); )
-			{
-				bool hasedges = false;
-				for (auto& e : m_nodes[*itr].edges)
-				{
-					if (m_nodes[e.dest_id].discovered_from == InvalidNode)
-					{
-						hasedges = true;
-						break;
-					}
-				}
+  constexpr double Grid_Margin_X = 0.05;
+  constexpr double Grid_Margin_Y = 0.05;
 
-				if (!hasedges)
-					itr = discovered.erase(itr);
-				else
-					++itr;
-			}
+  constexpr double Scaling = 10.0;
 
-			if (next != InvalidNode)
-			{
-				discovered.push_back(next);
-				m_nodes[next].discovered_from = selected;
-				m_nodes[next].path_metric = nextcost;
+  svg_out << "<svg width=\"" << Grid_Size_X * Grid_Spacing_X * Scaling
+          << "\" height=\"" << Grid_Size_Y * Grid_Spacing_Y * Scaling
+          << "\" xmlns=\"http://www.w3.org/2000/svg\">" << std::endl;
+  {
+    svg_out << "<g>" << std::endl;
+    for (const auto &n : m_nodes) {
+      const std::string &col =
+          is_warehouse(n.second.id)
+              ? "red"
+              : (is_internode(n.second.id) ? "blue" : "black");
 
-				if (next == to)
-					break;
-			}
+      svg_out << "<circle cx=\"" << static_cast<int>(n.second.x * Scaling)
+              << "\" cy=\"" << static_cast<int>(n.second.y * Scaling)
+              << "\" r=\"" << (is_warehouse(n.second.id) ? 3 : 2)
+              << "\" fill=\"" << col << "\"><title>" << n.second.id << " - "
+              << n.second.name << "</title></circle>" << std::endl;
 
-		}
+      for (const auto &e : n.second.edges) {
+        const Graph_Node &n1 = m_nodes[e.src_id];
+        const Graph_Node &n2 = m_nodes[e.dest_id];
 
-		if (m_nodes[to].discovered_from == InvalidNode)
-			return Path();
+        svg_out << "<line x1=\"" << static_cast<int>(n1.x * Scaling)
+                << "\" y1=\"" << static_cast<int>(n1.y * Scaling) << "\" x2=\""
+                << static_cast<int>(n2.x * Scaling) << "\" y2=\""
+                << static_cast<int>(n2.y * Scaling)
+                << "\" stroke-width=\"0.5\" stroke=\"" << col << "\" />"
+                << std::endl;
+      }
+    }
+    svg_out << "</g>" << std::endl;
+  }
+  svg_out << "</svg>" << std::endl;
 
-		Path pt;
-		pt.total_cost = m_nodes[to].path_metric;
+  m_pathSvg = svg_out.str();
 
-		pt.prepend(to);
-		
-		while (pt.node_ids.front() != from)
-			pt.prepend(m_nodes[pt.node_ids.front()].discovered_from);
-
-		return pt;
-	};
-
-	Path total;
-	total.total_cost = 0;
-
-	size_t curNode = inputNode;
-	for (size_t node : nodeIds)
-	{
-		auto res = find_path(curNode, node);
-		curNode = node;
-
-		if (res.empty())
-			continue;
-
-		total += res;
-	}
-
-	Path best = total;
-
-	m_path = best.node_ids;
-	m_totalCost = best.total_cost;
-
-	m_warehouses = std::set<size_t>(nodeIds.begin(), nodeIds.end());
-	m_internodes = std::set<size_t>(m_path.begin(), m_path.end());
-
-	return true;
-}
-
-bool CRouter::Draw_Path()
-{
-	if (m_warehouses.empty() || m_internodes.empty() || m_path.empty())
-		return false;
-
-	auto is_warehouse = [this](const size_t id) {
-		return (m_warehouses.find(id) != m_warehouses.end());
-	};
-	auto is_internode = [this](const size_t id) {
-		return (m_internodes.find(id) != m_internodes.end());
-	};
-
-	std::ostringstream svg_out;
-
-	constexpr size_t Grid_Size_X = 100;
-	constexpr size_t Grid_Size_Y = 100;
-
-	constexpr double Grid_Spacing_X = 1.0;
-	constexpr double Grid_Spacing_Y = 1.0;
-
-	constexpr double Grid_Margin_X = 0.05;
-	constexpr double Grid_Margin_Y = 0.05;
-
-	constexpr double Scaling = 10.0;
-
-	svg_out << "<svg width=\"" << Grid_Size_X * Grid_Spacing_X * Scaling << "\" height=\"" << Grid_Size_Y * Grid_Spacing_Y * Scaling << "\" xmlns=\"http://www.w3.org/2000/svg\">" << std::endl;
-	{
-		svg_out << "<g>" << std::endl;
-		for (const auto& n : m_nodes)
-		{
-			const std::string& col = is_warehouse(n.second.id) ? "red" : (is_internode(n.second.id) ? "blue" : "black");
-
-			svg_out << "<circle cx=\"" << static_cast<int>(n.second.x * Scaling) << "\" cy=\"" << static_cast<int>(n.second.y * Scaling) << "\" r=\"" << (is_warehouse(n.second.id) ? 3 : 2) << "\" fill=\"" << col << "\"><title>" << n.second.id << " - " << n.second.name << "</title></circle>" << std::endl;
-
-			for (const auto& e : n.second.edges)
-			{
-				const Graph_Node& n1 = m_nodes[e.src_id];
-				const Graph_Node& n2 = m_nodes[e.dest_id];
-
-				svg_out << "<line x1=\"" << static_cast<int>(n1.x * Scaling) << "\" y1=\"" << static_cast<int>(n1.y * Scaling) << "\" x2=\"" << static_cast<int>(n2.x * Scaling) << "\" y2=\"" << static_cast<int>(n2.y * Scaling) << "\" stroke-width=\"0.5\" stroke=\"" << col << "\" />" << std::endl;
-			}
-		}
-		svg_out << "</g>" << std::endl;
-	}
-	svg_out << "</svg>" << std::endl;
-
-	m_pathSvg = svg_out.str();
-
-	return true;
+  return true;
 }
