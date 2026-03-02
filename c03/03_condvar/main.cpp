5ec1e521708922e1661ee730804cdd8f6c504131
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-03-02 11:34:01 +0100

    c03-03 fix condition variable

--- c03/03_condvar/main.cpp
+++ c03/03_condvar/main.cpp
@@ -12,7 +12,9 @@ std::condition_variable condvar;
 // pracovni cislo - nejaky kus sdilene pameti
 int cislo;
 // ktere vlakno je ted "na tahu"? 1 nebo 2
-int turn = 1;
+volatile int turn = 1;
+
+volatile bool should_end = false;
 
 // prvni vlakno
 void Thread_Fnc_1() {
@@ -20,31 +22,37 @@ void Thread_Fnc_1() {
     std::unique_lock<std::mutex> lck(mutex);
 
     // pokud nejsme na tahu, pockame
-    while (turn != 1)
-      condvar.wait(lck);
+    // while (turn != 1 && !should_end)
+    //   condvar.wait(lck);
+
+    // ekvivalentni zapis
+    condvar.wait(lck, []() { return turn == 1 || should_end; });
+
+    if (should_end) {
+      condvar.notify_all();
+      std::cout << "1 konci..." << std::endl;
+      return;
+    }
 
     std::cout << "=== VLAKNO 1" << std::endl;
 
     std::cout << "Pracuji... ";
     // predstirame nejakou praci
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
     // potom udelame neco s cislem
-    if (cislo % 12 == 0)
-      cislo /= 2;
-    else if (cislo % 3 == 0)
-      cislo *= 12;
-    else if (cislo < 10)
-      cislo += 65;
-    else
-      cislo++;
+    cislo += 1;
 
     std::cout << "OK! cislo = " << cislo << std::endl;
+
+    if (cislo == 71) {
+      should_end = true;
+    }
 
     // prepneme "tah" na druhe vlakno a notifikujeme kohokoliv kdo ted spi nad
     // touto podminkovou promennou
     turn = 2;
-    condvar.notify_one();
+    condvar.notify_all();
   }
 }
 
@@ -52,27 +60,61 @@ void Thread_Fnc_2() {
   while (true) {
     std::unique_lock<std::mutex> lck(mutex);
 
-    while (turn != 2)
+    while (turn != 2 && !should_end)
       condvar.wait(lck);
+
+    if (should_end) {
+      condvar.notify_all();
+      std::cout << "2 konci..." << std::endl;
+      return;
+    }
 
     std::cout << "=== VLAKNO 2" << std::endl;
 
     std::cout << "Pracuji... ";
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
-    if (cislo > 10000)
-      cislo /= 500;
-    else if (cislo % 5 == 0)
-      cislo /= 5;
-    else if (cislo > 100)
-      cislo /= 2;
-    else
-      cislo++;
+    cislo += 2;
 
     std::cout << "OK! cislo = " << cislo << std::endl;
 
+    if (cislo == 71) {
+      should_end = true;
+    }
+
+    turn = 3;
+    condvar.notify_all();
+  }
+}
+
+void Thread_Fnc_3() {
+  while (true) {
+    std::unique_lock<std::mutex> lck(mutex);
+
+    while (turn != 3 && !should_end)
+      condvar.wait(lck);
+
+    if (should_end) {
+      condvar.notify_all();
+      std::cout << "3 konci..." << std::endl;
+      return;
+    }
+
+    std::cout << "=== VLAKNO 3" << std::endl;
+
+    std::cout << "Pracuji... ";
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    cislo += 3;
+
+    std::cout << "OK! cislo = " << cislo << std::endl;
+
+    if (cislo == 71) {
+      should_end = true;
+    }
+
     turn = 1;
-    condvar.notify_one();
+    condvar.notify_all();
   }
 }
 
@@ -81,9 +123,13 @@ int main(int argc, char **argv) {
 
   std::thread thr1(&Thread_Fnc_1);
   std::thread thr2(&Thread_Fnc_2);
+  std::thread thr3(&Thread_Fnc_3);
 
   thr1.join();
   thr2.join();
+  thr3.join();
+
+  std::cout << "korektni konec " << std::endl;
 
   return 0;
 }
