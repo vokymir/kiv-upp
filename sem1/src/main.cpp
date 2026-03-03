c68300c156f10ac027a822f8e17ae44cbc71d8c1
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-03-03 12:59:53 +0100

    edit main to better test loader

--- sem1/src/main.cpp
+++ sem1/src/main.cpp
@@ -1,22 +1,37 @@
 #include "loader.hpp"
 #include <cstddef>
 #include <print>
+#include <string>
 #include <string_view>
 
 using namespace std::literals;
 
 int main(int argc, char **argv) {
   bool parallel = false;
+  std::string dataset_path = "./data/";
 
   for (int i = 1; i < argc; ++i) {
     if (argv[i] == "--parallel"sv || argv[i] == "-p"sv) {
       parallel = true;
     }
+    if (argv[i] == "--s"sv) {
+      dataset_path += "small/";
+    }
+    if (argv[i] == "-m"sv) {
+      dataset_path += "medium/";
+    }
+    if (argv[i] == "-l"sv) {
+      dataset_path += "large/";
+    }
+  }
+  if (dataset_path.size() == 7) {
+    dataset_path += "small/";
   }
 
   std::print("{}\n", parallel ? "Is parallel" : "Is serial");
 
-  auto value = chmu::load_serial("./data/stanice.csv", "./data/mereni.csv");
+  auto value = chmu::load_serial(dataset_path + "stanice.csv",
+                                 dataset_path + "mereni.csv");
 
   std::print("stations size = {}\n", value->stations.size());
 
