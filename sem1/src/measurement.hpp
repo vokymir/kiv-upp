f4a5af06997bb90bef469fcacdfc6830e3d6e0d5
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-03-03 16:35:56 +0100

    use encoded date in measurements to save space

--- sem1/src/measurement.hpp
+++ sem1/src/measurement.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
+#include "convert.hpp"
 #include <charconv>
+#include <cstddef>
 #include <string_view>
 namespace chmu {
 
 class Measurement {
 private:
-  int year_;
-  int month_;
-  int day_;
+  size_t date_;
   int value_;
 
 public:
@@ -16,9 +16,14 @@ public:
               const std::string_view &day_sv,
               const std::string_view &value_sv) {
 
-    std::from_chars(year_sv.data(), year_sv.data() + year_sv.size(), year_);
-    std::from_chars(month_sv.data(), month_sv.data() + month_sv.size(), month_);
-    std::from_chars(day_sv.data(), day_sv.data() + day_sv.size(), day_);
+    int year = -1;
+    int month = -1;
+    int day = -1;
+    std::from_chars(year_sv.data(), year_sv.data() + year_sv.size(), year);
+    std::from_chars(month_sv.data(), month_sv.data() + month_sv.size(), month);
+    std::from_chars(day_sv.data(), day_sv.data() + day_sv.size(), day);
+
+    date_ = Date::from_date(year, month, day);
     std::from_chars(value_sv.data(), value_sv.data() + value_sv.size(), value_);
   }
 };
