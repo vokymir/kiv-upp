8493eb6881b1064bb66d5d1b29ec82d3f5c05f90
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-03-03 17:28:10 +0100

    auto/run - better for dataset size choosing

--- sem1/auto/run.sh
+++ sem1/auto/run.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-./bin/upp_sp1 ./data/small/stanice.csv ./data/small/mereni.csv --serial
+# Base path for datasets
+BASE="./data"
+
+# Default size is small
+SIZE="small"
+
+# Parse short flags for dataset size
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -s|--small) SIZE="small"; shift ;;
+    -m|--medium) SIZE="medium"; shift ;;
+    -l|--large) SIZE="large"; shift ;;
+    *) break ;; # stop parsing, rest are passed to program
+  esac
+done
+
+# Build file paths
+STANICE="$BASE/$SIZE/stanice.csv"
+MERENI="$BASE/$SIZE/mereni.csv"
+
+# Pass all remaining args as-is
+./bin/upp_sp1 "$STANICE" "$MERENI" "$@"
