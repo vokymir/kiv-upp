07c10147928ef850a88b3dad087bd61f55832917
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-03-03 15:22:43 +0100

    implement great main

--- sem1/src/main.cpp
+++ sem1/src/main.cpp
@@ -1,46 +1,109 @@
 #include "loader.hpp"
+#include "stations.hpp"
 #include <cstddef>
+#include <exception>
+#include <memory>
 #include <print>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 
 using namespace std::literals;
 
+constexpr std::string DEFAULT_STATIONS_PATH = "stanice.csv";
+constexpr std::string DEFAULT_MEASUREMENTS_PATH = "mereni.csv";
+constexpr bool DEFAULT_IS_PARALLEL = false;
+
+constexpr int ERROR_ARGUMENTS = 1;
+constexpr int ERROR_EXECUTION = 2;
+constexpr int ERROR_UNKNOWN = 3;
+
+void print_usage() {
+  std::print(
+      "\nUsage:\n./upp_sp1 [path to stations file] [path to measurements "
+      "file] [--serial | --parallel]\n\nThe defaults are:\n./upp_sp1 "
+      "{} {} --{}\n\nEither enter zero or all parameters.",
+      DEFAULT_STATIONS_PATH, DEFAULT_MEASUREMENTS_PATH,
+      DEFAULT_IS_PARALLEL ? "parallel" : "serial");
+}
+
+void load_args(int argc, char **argv, std::string &stations_path,
+               std::string &measurements_path, bool &is_parallel) {
+  // use provided parameters
+  if (argc == 4) {
+    stations_path = argv[1];
+    measurements_path = argv[2];
+
+    if (argv[3] == "--parallel"sv) {
+      is_parallel = true;
+    } else if (argv[3] == "--serial"sv) {
+      is_parallel = false;
+    } else {
+      throw std::runtime_error("Invalid serial/parallel flag.");
+    }
+
+  } else if (argc == 1) {
+    // use defaults
+
+  } else {
+    throw std::runtime_error("Invalid number of arguments.");
+  }
+}
+
+void serial_version(const std::string_view &stations_path,
+                    const std::string_view &measurements_path) {
+  // 0. load data
+  std::unique_ptr<chmu::Stations> stations =
+      chmu::load_serial(stations_path, measurements_path);
+
+  // 1. pre-process data (filtration)
+
+  // 2. identify fluctuation
+
+  // 3. calculate averages
+
+  // 4. draw a map for each month
+
+  // 5. create a CSV output file
+}
+
+void parallel_version(const std::string_view &stations_path,
+                      const std::string_view &measurements_path) {}
+
 int main(int argc, char **argv) {
-  bool parallel = false;
-  std::string dataset_path = "./data/";
+  std::string stations_path = DEFAULT_STATIONS_PATH;
+  std::string measurements_path = DEFAULT_MEASUREMENTS_PATH;
+  bool parallel = DEFAULT_IS_PARALLEL;
 
-  for (int i = 1; i < argc; ++i) {
-    if (argv[i] == "--parallel"sv || argv[i] == "-p"sv) {
-      parallel = true;
-    }
-    if (argv[i] == "--s"sv) {
-      dataset_path += "small/";
-    }
-    if (argv[i] == "-m"sv) {
-      dataset_path += "medium/";
-    }
-    if (argv[i] == "-l"sv) {
-      dataset_path += "large/";
-    }
-  }
-  if (dataset_path.size() == 7) {
-    dataset_path += "small/";
+  try {
+    load_args(argc, argv, stations_path, measurements_path, parallel);
+
+  } catch (const std::exception &ex) {
+    std::print("ERROR: {}\n", ex.what());
+    print_usage();
+    return ERROR_ARGUMENTS;
+
+  } catch (...) {
+    std::print("Unknown error happened...");
+    print_usage();
+    return ERROR_UNKNOWN;
   }
 
-  std::print("{}\n", parallel ? "Is parallel" : "Is serial");
+  try {
+    if (parallel) {
+      parallel_version(stations_path, measurements_path);
+    } else {
+      serial_version(stations_path, measurements_path);
+    }
 
-  auto value = chmu::load_serial(dataset_path + "stanice.csv",
-                                 dataset_path + "mereni.csv");
+  } catch (const std::exception &ex) {
+    std::print("ERROR: {}\n", ex.what());
+    return ERROR_EXECUTION;
 
-  std::print("stations size = {}\n", value->stations.size());
-
-  size_t sum = 0;
-  for (auto &s : value->stations) {
-    sum += s.measurements().size();
+  } catch (...) {
+    std::print("Unknown error happened...");
+    return ERROR_UNKNOWN;
   }
-
-  std::print("measurements size = {}\n", sum);
 
   return 0;
 }
