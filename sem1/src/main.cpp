039a576e08c28a230ea507aef1f4a4b0b26c84a1
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-03-04 10:40:38 +0100

    main: add timer

--- sem1/src/main.cpp
+++ sem1/src/main.cpp
@@ -3,6 +3,7 @@
 #include "fluctuation.hpp"
 #include "loader.hpp"
 #include "stations.hpp"
+#include <chrono>
 #include <exception>
 #include <memory>
 #include <print>
@@ -19,6 +20,19 @@ constexpr bool DEFAULT_IS_PARALLEL = false;
 constexpr int ERROR_ARGUMENTS = 1;
 constexpr int ERROR_EXECUTION = 2;
 constexpr int ERROR_UNKNOWN = 3;
+
+struct Timer {
+  using clock = std::chrono::high_resolution_clock;
+  clock::time_point start = clock::now();
+
+  void lap(const std::string_view &comment) {
+    auto now = clock::now();
+    auto elapsed =
+        std::chrono::duration_cast<std::chrono::milliseconds>(now - start);
+    std::print("[{}] {}\n", elapsed, comment);
+    start = now;
+  }
+};
 
 void print_usage() {
   std::print(
@@ -58,20 +72,24 @@ void print_info(const chmu::Stations &sts) {
 
 void serial_version(const std::string_view &stations_path,
                     const std::string_view &measurements_path) {
+  Timer timer;
+
   // 0. load data
   std::unique_ptr<chmu::Stations> stations =
       chmu::load__serial(stations_path, measurements_path);
+  timer.lap("Data loaded.");
 
   // 1. pre-process data (filtration)
   chmu::filter__serial(*stations);
+  timer.lap("Data filtered.");
 
   // 3. calculate averages
-
   chmu::compute_averages__serial(*stations);
+  timer.lap("Averages computed.");
 
   // 2. identify fluctuation
-
   chmu::identify_fluctuation__serial(*stations);
+  timer.lap("Fluctuations identified.");
 
   // 4. draw a map for each month
 
