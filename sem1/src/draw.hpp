57a48381689515b74d5d373be1d88bc8a4cd639e
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-03-04 10:43:19 +0100

    starting draw

---
+++ sem1/src/draw.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "stations.hpp"
+namespace chmu {
+
+void draw_svg__serial(const Stations &stations);
+
+void draw_svg_month__serial(const Stations &stations);
+
+} // namespace chmu
