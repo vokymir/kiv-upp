37ba5a87e5889bf0cd7304cfd38b04994a3a7900
Vokymir <voky.consultant@gmail.com>
2026-03-31 14:35:27 +0200

    fix: critical bug in collect_stats, wrong was stats collected

--- sem1/src/collect_stats.cpp
+++ sem1/src/collect_stats.cpp
@@ -1,6 +1,8 @@
 #include "collect_stats.hpp"
 #include "model/model.hpp"
+#include <algorithm>
 #include <cstddef>
+#include <iterator>
 #include <numeric>
 #include <ranges>
 namespace chmu {
@@ -21,13 +23,15 @@ void collect_stats_station__serial(Station &station) {
                a.date().month_ == b.date().month_;
       });
 
-  // calculate everything from that month
+  // store month average for each month
   for (auto month_view : month_year_groups) {
     collect_stats_station_month__serial(station, month_view);
   }
 
+  collect_stats_station_minmax__serial(station.stats());
+
   // final - calculate mean
-  collect_stats_station_means__serial(station);
+  collect_stats_station_means__serial(station.stats());
 }
 
 void collect_stats_station_month__serial(Station &station, auto month_view) {
@@ -36,43 +40,33 @@ void collect_stats_station_month__serial(Station &station, auto month_view) {
   int month = date.month_;
   int year = date.year_;
 
-  // ===
-  // find all stats
-  float sum = 0;
-  float min_val = std::numeric_limits<float>::infinity();
-  float max_val = -1 * std::numeric_limits<float>::infinity();
-  size_t days = 0;
-
-  for (const Measurement &m : month_view) {
-    float value = m.value();
-    sum += value;
-    min_val = min_val < value ? min_val : value;
-    max_val = max_val > value ? max_val : value;
-    ++days;
-  }
-
-  // if EMPTY STATS
+  size_t days = std::distance(month_view.begin(), month_view.end());
   if (days == 0) {
     return;
   }
 
-  // ===
-  // save all stats
-  Stats &s = station.stats();
-  float avg = sum / days;
+  float avg = std::accumulate(month_view.begin(), month_view.end(), 0.0f,
+                              [](float acc, const Measurement &m) {
+                                return acc + m.value();
+                              }) /
+              days;
 
-  s.monthly_series_[month - 1].add(avg, year);
-
-  s.monthly_min_[month - 1] = std::min(s.monthly_min_[month - 1], min_val);
-  s.monthly_max_[month - 1] = std::max(s.monthly_max_[month - 1], max_val);
-
-  s.global_min_ = std::min(s.global_min_, min_val);
-  s.global_max_ = std::max(s.global_max_, max_val);
+  station.stats().monthly_series_[month - 1].add(avg, year);
 }
 
-void collect_stats_station_means__serial(Station &station) {
-  auto &stats = station.stats();
+void collect_stats_station_minmax__serial(Stats &stats) {
+  for (size_t i = 0; i < 12; ++i) {
+    for (const auto ma : stats.monthly_series_[i].values) {
+      stats.monthly_min_[i] = std::min(stats.monthly_min_[i], ma.value);
+      stats.monthly_max_[i] = std::max(stats.monthly_max_[i], ma.value);
+    }
 
+    stats.global_min_ = std::min(stats.global_min_, stats.monthly_min_[i]);
+    stats.global_max_ = std::max(stats.global_max_, stats.monthly_max_[i]);
+  }
+}
+
+void collect_stats_station_means__serial(Stats &stats) {
   for (size_t i = 0; i < 12; ++i) {
     const auto &series = stats.monthly_series_[i].values;
 
--- sem1/src/collect_stats.hpp
+++ sem1/src/collect_stats.hpp
@@ -11,12 +11,15 @@ void collect_stats__serial(std::vector<Station> &stations);
 // compute stats for one station, all years&months
 void collect_stats_station__serial(Station &station);
 
-// compute for one station, one month, one year
+// fill month_series for one station, one month, one year
 // the month_view must contain all measurements from one particular year and
 // month
 void collect_stats_station_month__serial(Station &station, auto month_view);
 
+// calculate montly/global min/max for one station with filled month_series
+void collect_stats_station_minmax__serial(Stats &stats);
+
 // compute all means in one station
-void collect_stats_station_means__serial(Station &station);
+void collect_stats_station_means__serial(Stats &stats);
 
 } // namespace chmu
