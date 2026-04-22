abb996b6171b9cd98789e6f6f3ab78db97904f4f
Vokymir <voky.consultant@gmail.com>
2026-04-22 14:29:59 +0200

    update run script to work

--- sem2/auto/run.sh
+++ sem2/auto/run.sh
@@ -2,5 +2,10 @@
 
 DIR="bin"
 BIN="UPP_SP2"
+HOSTFILE=".hostfile"
+NP=10 # total number of processes
+N=3 # worker A
+M=2 # worker B
 
-mpirun "./${DIR}/${BIN}"
+mpirun --oversubscribe -np "${NP}" --hostfile "${HOSTFILE}" \
+  "./${DIR}/${BIN}" -n "${N}" -m "${M}"
