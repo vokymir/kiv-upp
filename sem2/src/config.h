95b4d7ebe9dc1df556e27099c700b34fd9ec35ac
Vokymir <voky.consultant@gmail.com>
2026-04-23 13:59:51 +0200

    comment config

--- sem2/src/config.h
+++ sem2/src/config.h
@@ -1,17 +1,26 @@
 #pragma once
 
-// global configuration - because passing N,M was messy
+// global configuration + useful getters
+// It could have be done without global config, but this way it is much more
+// convenient - and it is only setting N and M once in main.cpp/main. If these
+// values were to change multiple times, I wouldn't do it.
 namespace cfg {
 
+// number of workers A
 inline int N = 0;
+
+// number of workers B for each worker A
 inline int M = 0;
 
-// Example for N=3, M=2
+// Example distribution for N=3, M=2
 // rank:      0  1  2  3  4  5  6  7  8  9
 // role:      M  A1 A2 A3 B1 B2 B3 B4 B5 B6
 // owner:     -  M  M  M  A1 A1 A2 A2 A3 A3
 
 // == HARD CONFIG ==
+// if things are sensibly changed here, the rest will still work (and therefore
+// the whole program)
+// it only needs both A and B to have continuous indices
 
 inline int idx_master() { return 0; }
 
@@ -22,6 +31,7 @@ inline int idx_B_first() { return N + 1; }
 inline int idx_B_last() { return N + N * M; }
 
 // == USEFUL GETTERS ==
+// useful when assigning work to every node/worker
 
 inline bool is_master(int rank) { return rank == idx_master(); }
 
@@ -43,16 +53,19 @@ inline int assign_A(int i) { return idx_A_first() + (i % N); }
 // i is some iteration, so it is fairly distributed
 inline int assign_B(int rank_A, int i) {
   // how much offset because of A
-  int offset_of_A_group = (rank_A - idx_A_first()) * M;
+  int group_offset = (rank_A - idx_A_first()) * M;
   // start index of A's group
-  int start_of_group = idx_B_first() + offset_of_A_group;
+  int group_start = idx_B_first() + group_offset;
   // don't overshoot the group
-  int employee = start_of_group + (i % M);
+  int employee = group_start + (i % M);
 
   return employee;
 }
 
 // who to send direct message if I am done? whom to listen to?
+// master -> -1
+// A -> master
+// B -> correct A
 inline int employer(int rank) {
   if (is_master(rank)) {
     return -1;
